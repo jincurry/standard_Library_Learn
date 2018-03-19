0a0df8a16fdda64663719d20cf2ebc3433826f39
jincurry <jintao422516@gmail.com>
2018-03-19 15:30:06 +0800

    glob examples

---
+++ the_file_system/glob/glob_maketestdata.py
@@ -0,0 +1,25 @@
+import os
+
+
+def mkfile(filename):
+    print(filename)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('\n')
+
+
+print('dir')
+os.mkdir('dir')
+
+mkfile('dir/file.txt')
+mkfile('dir/file1.txt')
+mkfile('dir/file2.txt')
+mkfile('dir/filea.txt')
+mkfile('dir/fileb.txt')
+mkfile('dir/file*.txt')
+mkfile('dir/file?.txt')
+mkfile('dir/file[.txt')
+
+print('dir/subdir')
+os.mkdir('dir/subdir')
+
+mkfile('dir/subdir/subfile.txt')
